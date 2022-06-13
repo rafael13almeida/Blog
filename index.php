3367f92da781259f6c5f9cf839702df46fbcd1be
Rafael Almeida <rafael13almeida@gmail.com>
2022-06-13 19:41:02 -0300

    Iniciando o projeto

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Estilo -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+    <title>Blog - Hora do PHP</title>
+    
+</head>
+<body>
+    <h1>Testing</h1>
+</body>
+</html>
