bcb2f337c646642d583b404ae3ac326c92c12fac
Rafael Almeida <rafael13almeida@gmail.com>
2022-06-15 10:57:20 -0300

    Atualizando home

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 include_once("templates/header.php");
 ?>
-<h1>Testing</h1>
 
+<main>
+    <div id="titulo-container">
+        <h1>Blog do Fael</h1>
+        <p>Estudar muito para saber um pouco ;)</p>
+    </div>
+    <div id="post-container">
+        <?php foreach($posts as $post): ?>
+            <div id="post-box">
+                <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['titulo'] ?>">
+                <h2 class="post-titulo">
+                    <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"> <?= $post['titulo'] ?></a>
+                </h2>
+                <p class="post-descricao"><?= $post['descricao'] ?></p>
+                <div class="tags-container">
+                <?php foreach($post['tags'] as $tag): ?>
+                    <a href="#"><?=$tag?></a>
+                <?php endforeach;?>    
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</main>
 <?php
 include_once("templates/footer.php");
 ?>
