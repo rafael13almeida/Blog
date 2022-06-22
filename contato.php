69dc655f4bbccb819383ad0d83c3ef03184e2a7b
Rafael Almeida <rafael13almeida@gmail.com>
2022-06-22 19:23:42 -0300

    Atualizando Contato

--- contato.php
+++ contato.php
@@ -1,15 +1,28 @@
 <?php
 include_once("templates/header.php");
 ?>
-    <h1>Contato</h1>
-    <div>
+<h1>Contato</h1>
+
+<div id="contato-email">
+    <h3>E-mail de Contato</h3>
     <p>
         email - rafael13almeida@gmail.com
     </p>
-    </div>
+</div>
+
+<div id="contato-linkedin">
+    <h3>LinkedIn</h3>
     <p>
-         <a href="https://www.linkedin.com/in/rafael-almeida-287605137/">Linkedin</a>
+        <a href="https://www.linkedin.com/in/rafael13almeida/">Linkedin</a>
     </p>
+</div>
+
+<div id="contato-github">
+    <h3>Github</h3>
+    <p>
+        <a href="https://github.com/rafael13almeida">Github</a>
+    </p>
+</div>
 
 <?php
 include_once("templates/footer.php");
